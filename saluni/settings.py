270abe0a936fd40908529ea36bec0f61c537f665
Jina Lako <emailyako@example.com>
2025-10-02 20:55:59 +0300

    homepage

--- saluni/settings.py
+++ saluni/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'saluni_kike',
     'saluni_kiume',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
--- saluni/urls.py
+++ saluni/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include  # 👈 ONGEZA 'include'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('saluni_kike.urls')),  # 👈 ONGEZA HII LINE
+    path('', include('saluni_kike.urls')), 
+    path('', include('dashboard.urls')),   
 ]
